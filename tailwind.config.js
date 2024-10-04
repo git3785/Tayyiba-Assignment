7f7dedd9452dc779e481bd7adeb4c48e3ce3cecd
git3785 <gitcourse3@gmail.com>
2024-10-04 05:41:34 +0500

    Next.js and Tailwind CSS project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',  // Make sure these paths are correct
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
